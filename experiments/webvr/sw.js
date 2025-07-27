12d1eddab32d7785da9239a665df430d69b6d367
groybe <8501882+groybe@users.noreply.github.com>
2025-07-27 12:12:16 +1000

    test of webvr

---
+++ experiments/webvr/sw.js
@@ -0,0 +1,50 @@
+self.addEventListener("install", event => {
+		console.log('Service Worker installing...');
+    event.waitUntil(
+        caches.open("butterchurn-cache").then(cache => {
+            return cache.addAll([
+                "./pwavr.html",
+                "./manifest.json",
+                "./libs/aframe.min.js",
+                "./libs/base.min.js",
+                "./libs/butterchurn.js",
+                "./libs/extra.min.js",
+                "./libs/jquery-3.1.1.min.js",
+                "./libs/lodash.js",
+                "./libs/normalize.css",
+                "./favicon.ico",
+                "./logo192.png",
+                "./logo512.png"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", event => {
+		console.log('Fetching:', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'REFRESH_CACHE') {
+		console.log('Service Worker refreshing cache...');
+    caches.open('butterchurn-cache').then(cache => {
+      return cache.addAll([
+                "./pwavr.html",
+                "./manifest.json",
+                "./libs/aframe.min.js",
+                "./libs/base.min.js",
+                "./libs/butterchurn.js",
+                "./libs/extra.min.js",
+                "./libs/jquery-3.1.1.min.js",
+                "./libs/lodash.js",
+                "./libs/normalize.css",
+                "./favicon.ico",
+                "./logo192.png",
+                "./logo512.png"
+      ]);
+    });
+  }
+});
